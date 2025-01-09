7d27f3c9378aea2961f49829b13c0bc113a782b6
liujunyi <liujunyi@proitav.com>
2025-01-09 15:38:55 +0800

    [Improve]:增加extern "C"，防止C++访问C语言函数时name mangling;

--- drv_ps186.h
+++ drv_ps186.h
@@ -1,6 +1,10 @@
 #ifndef __DRV_PS186_H__
 #define __DRV_PS186_H__
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 #include "ps186.h"
 
 int8_t ps186_register(struct ps186_dev *ptPS186Dev, char *i_pName);
@@ -9,6 +13,10 @@ int8_t ps186_open(struct ps186_dev *i_pdev);
 int8_t ps186_close(struct ps186_dev *i_pdev);
 int8_t ps186_control(struct ps186_dev *i_pdev, uint16_t cmd, void *arg);
 
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
